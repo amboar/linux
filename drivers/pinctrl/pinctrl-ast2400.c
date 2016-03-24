ac8272b00917319d608f952b41c21b35c825c336
Andrew Jeffery <andrew@aj.id.au>
2016-03-24 17:36:08 +1030

    HAACKS: Macro magic and DSL for pinmux
    Signed-off-by: Andrew Jeffery <andrew@aj.id.au>

---
+++ drivers/pinctrl/pinctrl-ast2400.c
@@ -0,0 +1,261 @@
+/*
+ * Copyright (C) 2016 IBM
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation version 2.
+ *
+ * This program is distributed "as is" WITHOUT ANY WARRANTY of any
+ * kind, whether express or implied; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+#include <asm/io.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/of.h>
+#include <linux/platform_device.h>
+#include <linux/pinctrl/pinctrl.h>
+#include <linux/pinctrl/pinmux.h>
+#include <linux/pinctrl/pinconf.h>
+#include <linux/types.h>
+#include "core.h"
+
+struct ast_ctrl_desc {
+	bool (*op)(void __iomem *, struct ast_ctrl_desc *);
+	unsigned reg;
+	uint32_t mask;
+	uint32_t val;
+};
+
+static bool pin_expr_eq(void __iomem *base, struct ast_ctrl_desc *desc)
+{
+	uint32_t val = ioread32(base + desc->reg) & desc->mask;
+	return val == desc->val;
+}
+
+static bool pin_expr_neq(void __iomem *base, struct ast_ctrl_desc *desc)
+{
+	return !pin_expr_eq(base, desc);
+}
+
+struct ast_pin_expr {
+	bool (*op)(void __iomem *, struct ast_pin_expr *);
+	int ndescs;
+	struct ast_ctrl_desc descs[];
+};
+
+static bool pin_expr_and(void __iomem *base, struct ast_pin_expr *expr)
+{
+	bool ret = true;
+	int i;
+
+	for (i = 0; i < expr->ndescs; i++) {
+		struct ast_ctrl_desc *desc = &expr->descs[i];
+		ret &= desc->op(base, desc);
+	}
+	return ret;
+}
+
+static bool pin_expr_or(void  __iomem *base, struct ast_pin_expr *expr)
+{
+	bool ret = false;
+	int i;
+
+	for (i = 0; i < expr->ndescs; i++) {
+		struct ast_ctrl_desc *desc = &expr->descs[i];
+		ret |= desc->op(base, desc);
+	}
+	return ret;
+}
+
+struct ast_pinctrl_desc {
+	struct ast_pin_expr *f1;
+	struct ast_pin_expr *f2;
+};
+
+#define CTRL_HIGH_PRIO 1
+#define CTRL_LOW_PRIO 2
+
+#define AST_CTRL_DESC(_op, _reg, _mask, _val) \
+	{ .op = _op, .reg = _reg, .mask = _mask, .val = _val }
+
+#define AST_CTRL_DESC_EQ(_reg, _mask, _val) \
+	AST_CTRL_DESC(pin_expr_eq, _reg, _mask, _val)
+
+#define AST_CTRL_DESC_NEQ(_reg, _mask, _val) \
+	AST_CTRL_DESC(pin_expr_neq, _reg, _mask, _val)
+
+#define _AST_PIN_EXPR_OP(_ball, _fn, _op, _ndescs, ...) \
+	static struct ast_pin_expr pin_expr_f##_fn##_##_ball = { \
+		.op = _op, .ndescs = _ndescs, .descs = { __VA_ARGS__, }, \
+	}
+
+#define AST_PIN_EXPR_OP(_ball, _fn, _op, _ndescs, ...) \
+	_AST_PIN_EXPR_OP(_ball, _fn, _op, _ndescs, __VA_ARGS__)
+
+#define AST_PIN_EXPR(_ball, _fn, ...) \
+	AST_PIN_EXPR_OP(_ball, _fn, NULL, 1, __VA_ARGS__)
+
+#define _AST_PIN_MF(_ball, _f1, _f2) \
+	static struct ast_pinctrl_desc ball_##_ball = { .f1 = _f1, .f2 = _f2, }
+
+#define AST_PIN_MF(_ball) \
+	_AST_PIN_MF(_ball, &pin_expr_f1_##_ball, &pin_expr_f2_##_ball)
+
+#define AST_PIN_SF_OP(_ball, _fn, _op, _ndescs, ...) \
+	AST_PIN_EXPR_OP(_ball, _fn, _op, _ndescs, __VA_ARGS__); \
+	_AST_PIN_MF(_ball, &pin_expr_f##_fn##_##_ball, NULL)
+
+#define AST_PIN_SF(_ball, ...) \
+	AST_PIN_SF_OP(_ball, 1, NULL, 1, __VA_ARGS__)
+
+#define SCU3C 0x3C
+#define SCU3C 0x3C
+#define SCU70 0x70
+#define SCU80 0x80
+#define SCU84 0x84
+#define SCU88 0x88
+#define SCU8C 0x8C
+#define SCU90 0x90
+#define SCU94 0x94
+
+AST_PIN_SF(D6, AST_CTRL_DESC_EQ(SCU80, BIT_MASK(0), 1));
+AST_PIN_SF(B5, AST_CTRL_DESC_EQ(SCU80, BIT_MASK(1), 1));
+AST_PIN_SF(A4, AST_CTRL_DESC_EQ(SCU80, BIT_MASK(2), 1));
+AST_PIN_SF(E6, AST_CTRL_DESC_EQ(SCU80, BIT_MASK(3), 1));
+
+AST_PIN_EXPR(B4, CTRL_HIGH_PRIO, AST_CTRL_DESC_EQ(SCU90, BIT_MASK(22), 1));
+AST_PIN_EXPR(B4, CTRL_LOW_PRIO, AST_CTRL_DESC_EQ(SCU80, BIT_MASK(5), 1));
+AST_PIN_MF(B4);
+
+AST_PIN_EXPR(A3, CTRL_HIGH_PRIO, AST_CTRL_DESC_EQ(SCU90, BIT_MASK(2), 1));
+AST_PIN_EXPR(A3, CTRL_LOW_PRIO, AST_CTRL_DESC_EQ(SCU80, BIT_MASK(6), 1));
+AST_PIN_MF(A3);
+
+AST_PIN_EXPR(D5, CTRL_HIGH_PRIO, AST_CTRL_DESC_EQ(SCU90, BIT_MASK(2), 1));
+AST_PIN_EXPR(D5, CTRL_LOW_PRIO, AST_CTRL_DESC_EQ(SCU80, BIT_MASK(7), 1));
+AST_PIN_MF(D5);
+
+AST_PIN_SF(J21, AST_CTRL_DESC_EQ(SCU80, BIT_MASK(8), 1));
+AST_PIN_SF(J20, AST_CTRL_DESC_EQ(SCU80, BIT_MASK(9), 1));
+AST_PIN_SF(H18, AST_CTRL_DESC_EQ(SCU80, BIT_MASK(10), 1));
+AST_PIN_SF(F18, AST_CTRL_DESC_EQ(SCU80, BIT_MASK(11), 1));
+
+AST_PIN_EXPR(C5, CTRL_HIGH_PRIO, AST_CTRL_DESC_EQ(SCU90, BIT_MASK(22), 1));
+AST_PIN_EXPR(C5, CTRL_LOW_PRIO, AST_CTRL_DESC_EQ(SCU80, BIT_MASK(4), 1));
+AST_PIN_MF(C5);
+
+AST_PIN_EXPR(A18, CTRL_HIGH_PRIO, AST_CTRL_DESC_EQ(SCU90, BIT_MASK(1), 1));
+AST_PIN_EXPR_OP(A18, CTRL_LOW_PRIO, pin_expr_or, 2,
+	       	AST_CTRL_DESC_EQ(SCU8C, BIT_MASK(1), 1),
+		AST_CTRL_DESC_EQ(SCU70, BIT_MASK(21), 1));
+AST_PIN_MF(A18);
+
+struct ast2400_pin_function {
+	const char *name;
+	const char * const *groups;
+	const unsigned ngroups;
+};
+
+static const struct pinctrl_pin_desc ast2400_pinctrl_pins[] = {
+
+};
+
+struct ast2400_pinctrl_data {
+	void __iomem *reg_base;
+
+	const struct pinctrl_pin_desc *pins;
+	const unsigned npins;
+
+	const struct ast2400_pin_function *functions;
+	const unsigned nfunctions;
+};
+
+static struct ast2400_pinctrl_data ast2400_pinctrl = {
+	.pins = ast2400_pinctrl_pins,
+	.npins = ARRAY_SIZE(ast2400_pinctrl_pins),
+	/*
+	.functions = NULL,
+	.nfunctions = ARRAY_SIZE(NULL),
+	*/
+};
+
+
+
+static struct pinctrl_ops ast2400_pinctrl_ops = {
+	.get_groups_count = NULL,
+	.get_group_name = NULL,
+	.get_group_pins = NULL,
+	.pin_dbg_show = NULL,
+	.dt_node_to_map = NULL,
+	.dt_free_map = NULL,
+};
+
+static struct pinmux_ops ast2400_pinmux_ops = {
+	.get_functions_count = NULL,
+	.get_function_name = NULL,
+	.get_function_groups = NULL,
+	.set_mux = NULL,
+};
+
+static struct pinconf_ops ast2400_pinconf_ops = {
+	.pin_config_get = NULL,
+	.pin_config_set = NULL,
+};
+
+static struct pinctrl_desc ast2400_pinctrl_desc = {
+	.pctlops = &ast2400_pinctrl_ops,
+	.pmxops = &ast2400_pinmux_ops,
+	.confops = &ast2400_pinconf_ops,
+	.owner = THIS_MODULE,
+};
+
+static int __init ast2400_pinctrl_probe(struct platform_device *pdev)
+{
+	struct ast2400_pinctrl_data *pdata = &ast2400_pinctrl;
+	struct resource *res;
+	struct pinctrl_dev *pctl;
+
+	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
+	pdata->reg_base = devm_ioremap_resource(&pdev->dev, res);
+	if (IS_ERR(pdata->reg_base)) {
+		dev_err(&pdev->dev, "Failed to ioremap MEM resource\n");
+		return -ENODEV;
+	}
+
+	/* FIXME: do some dynamic stuff? See pinctrl-bcm281xx.c */
+
+	ast2400_pinctrl_desc.name = dev_name(&pdev->dev);
+	ast2400_pinctrl_desc.pins = ast2400_pinctrl.pins;
+	ast2400_pinctrl_desc.npins = ast2400_pinctrl.npins;
+
+	pctl = pinctrl_register(&ast2400_pinctrl_desc, &pdev->dev, pdata);
+
+	if (IS_ERR(pctl)) {
+		dev_err(&pdev->dev, "Failed to register pinctrl\n");
+		return PTR_ERR(pctl);
+	}
+	
+	platform_set_drvdata(pdev, pdata);
+
+	return 0;
+}
+
+static const struct of_device_id ast2400_pinctrl_of_match[] = {
+	{ .compatible = "aspeed,ast2400-pinctrl", },
+	{ },
+};
+
+static struct platform_driver ast2400_pinctrl_driver = {
+	.driver = {
+		.name = "ast2400-pinctrl",
+		.of_match_table = ast2400_pinctrl_of_match,
+	},
+};
+
+module_platform_driver_probe(ast2400_pinctrl_driver, ast2400_pinctrl_probe);
+
+MODULE_AUTHOR("Andrew Jeffery <andrew@aj.id.au>");
+MODULE_DESCRIPTION("ASPEED AST2400 pinctrl driver");
+MODULE_LICENSE("GPL v2");
