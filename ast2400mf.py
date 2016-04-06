7fc4890df616143327a7227ee2e4fae27ed79e46
Andrew Jeffery <andrew@aj.id.au>
2016-04-06 14:25:03 +0930

    HACKS: ast2400mf: Combine signals on a pin in network print output
    Signed-off-by: Andrew Jeffery <andrew@aj.id.au>

--- ast2400mf.py
+++ ast2400mf.py
@@ -11,12 +11,25 @@ bop_t = nt("bop_t", "type a b")
 sig_t = nt("sig_t", "sig expr")
 pin_t = nt("pin", "pin default high low other")
 
+def get_mask_bits(mask):
+    if "," in mask:
+        bits = set()
+        for disjoint in mask.split(","):
+            bits.update(get_mask_bits(disjoint))
+        return bits
+    if ":" in mask:
+        r = [ int(x) for x in mask.split(":") ]
+        s = set(range(r[1], r[0] + 1))
+        return s
+    return set( [ int(mask) ] )
+
 def parse_pd(toks):
     pat = r"^(SCU[A-F0-9]+|Strap|SIORD_30)\[([0-9:,]+)\]"
     mat = re.search(pat, toks[0])
     if not mat:
         return None, toks
-    pd = pd_t(mat.group(1), mat.group(2))
+    bits = get_mask_bits(mat.group(2))
+    pd = pd_t(mat.group(1), frozenset(bits))
     _toks = [ toks[0].replace(mat.group(0), "") ]
     _toks.extend(toks[1:])
     return pd, _toks
@@ -89,7 +102,7 @@ def gen_pins(src, filt=None):
         d["other"] = toks[-1]
         d["high"], toks = parse_sig(toks[2:-1])
         d["low"], toks = parse_sig(toks)
-        if filt and d["default"] not in filt:
+        if filt and d["other"] not in filt:
             continue
         pins.append(pin_t(**d))
     return pins
@@ -100,108 +113,92 @@ def is_simple(expr):
 def is_compound(expr):
     return not is_simple(expr)
 
-def get_simple_bits(expr):
+def get_simple_pds(expr):
     if is_simple(expr):
         return [ expr.a ]
     assert is_compound(expr)
     bits = []
     assert expr.a
-    bits.extend(get_simple_bits(expr.a))
+    bits.extend(get_simple_pds(expr.a))
     assert expr.b
-    bits.extend(get_simple_bits(expr.b))
+    bits.extend(get_simple_pds(expr.b))
     return bits
 
-def get_compound_bits(expr):
+def get_compound_pds(expr):
     assert expr
     if is_simple(expr):
         return []
     assert is_compound(expr)
-    return get_simple_bits(expr)
+    return get_simple_pds(expr)
 
-def get_all_compound_bits(pins):
-    all_bits = []
+def get_all_compound_pds(pins):
+    all_pds = []
     for pin in pins:
         if not pin.high.expr:
             pp(pin)
         assert pin.high.expr
-        iters = [ (pin.high, get_compound_bits(pin.high.expr)) ]
+        iters = [ (pin.high, get_compound_pds(pin.high.expr)) ]
         if pin.low:
             if not pin.low.expr:
                 pp(pin)
             assert pin.low.expr
-            iters.append((pin.low, get_compound_bits(pin.low.expr)))
+            iters.append((pin.low, get_compound_pds(pin.low.expr)))
         src = [ zip(repeat(sig), bits) for sig, bits in iters ]
         for sig, bits in chain(*src):
-            if bits not in all_bits:
-                all_bits.append(bits)
-    return all_bits
+            if bits not in all_pds:
+                all_pds.append(bits)
+    return all_pds
 
-def get_bits(expr):
-    bits = []
+def get_pds(expr):
+    pds = []
     if not expr:
-        return bits
+        return pds
     if expr.type in ["=", "!="]:
-        bits.append((expr.a))
+        pds.append((expr.a))
     else:
-        bits.extend(get_bits(expr.a))
-        bits.extend(get_bits(expr.b))
-    return bits
+        pds.extend(get_pds(expr.a))
+        pds.extend(get_pds(expr.b))
+    return pds
 
-def get_all_bits(pins):
-    all_bits = {}
+def get_all_pds(pins):
+    all_pds = {}
     for pin in pins:
-        iters = [ (pin.high, get_bits(pin.high.expr)) ]
+        iters = [ (pin.high, get_pds(pin.high.expr)) ]
         if pin.low:
-            iters.append((pin.low, get_bits(pin.low.expr)))
-        src = [ zip(repeat(sig), bits) for sig, bits in iters ]
-        for sig, bits in chain(*src):
-            if bits not in all_bits:
-                all_bits[bits] = []
-            t = (pin.default, sig.sig)
-            if t not in all_bits[bits]:
-                all_bits[bits].append(t)
-    return all_bits
+            iters.append((pin.low, get_pds(pin.low.expr)))
+        src = [ zip(repeat(sig), pds) for sig, pds in iters ]
+        for sig, pds in chain(*src):
+            if pds not in all_pds:
+                all_pds[pds] = []
+            t = (pin.other, sig.sig)
+            if t not in all_pds[pds]:
+                all_pds[pds].append(t)
+    return all_pds
 
-def print_bits(bits):
-    for k in sorted(bits.keys(), key=lambda x: len(bits[x]), reverse=True):
-        print("{} ; {}".format(k, sorted(bits[k])))
+def print_pds(pds):
+    for k in sorted(pds.keys(), key=lambda x: len(pds[x]), reverse=True):
+        print("{} ; {}".format(k, sorted(pds[k])))
 
-def get_mask_bits(mask):
-    print(mask)
-    if None is mask:
-        return set()
-    bits = []
-    if "," in mask:
-        for disjoint in mask.split(","):
-            sbits = get_mask_bits(disjoint)
-            print(sbits)
-            bits.extend(sbits)
-        return set(bits)
-    if ":" in mask:
-        r = [ int(x) for x in mask.split(":") ]
-        bits.extend(range(r[0], r[1] + 1))
-        return set(bits)
-    bits.append(int(mask))
-    return set(bits)
-
-def expr_uses_bit(expr, bit):
+def expr_uses_bits(expr, bit):
     if is_simple(expr):
-        a = get_mask_bits(expr.a.mask)
-        b = get_mask_bits(bit.mask)
-        return set() != a.intersection(b) and expr.a.reg == bit.reg
+        return (set() != expr.a.mask.intersection(bit.mask)
+                and expr.a.reg == bit.reg)
     assert is_compound(expr)
-    return expr_uses_bit(expr.a, bit) or expr_uses_bit(expr.b, bit)
+    return expr_uses_bits(expr.a, bit) or expr_uses_bits(expr.b, bit)
 
-def sig_uses_bit(sig, bit):
-    return expr_uses_bit(sig.expr, bit)
+def sig_uses_bits(sig, bit):
+    return expr_uses_bits(sig.expr, bit)
 
 def get_bit_sigs(pins, bit):
     sigs = []
     for pin in pins:
-        if sig_uses_bit(pin.high, bit):
+        if not pin.high.expr:
+            print(pin)
+            sys.exit(1)
+        if sig_uses_bits(pin.high, bit):
             sigs.append(pin.high)
         if pin.low:
-            if sig_uses_bit(pin.low, bit):
+            if sig_uses_bits(pin.low, bit):
                 sigs.append(pin.low)
     return sigs
 
@@ -210,22 +207,36 @@ def get_sig_pin(pins, sig):
         if sig in [ pin.high, pin.low ]:
             return pin
 
+def explode_pd(pd):
+    return [ pd_t(pd.reg, frozenset([bit])) for bit in pd.mask ]
+
+def gen_bit_sigs_lookup(pds):
+    lookup = {}
+    for pd in pds:
+        sigs = get_bit_sigs(pins, pd)
+        for bit in explode_pd(pd):
+            lookup[bit] = sigs
+    return lookup
+
 def find_networks(pins):
-    cbits = get_all_compound_bits(pins)
-    bsigs = { cbit : get_bit_sigs(pins, cbit) for cbit in cbits }
+    pds = get_all_pds(pins)
+    bsigs = gen_bit_sigs_lookup(pds)
     nets = []
-    for bit in cbits:
-        if bit not in bsigs:
-            continue
-        q = bsigs[bit][:]
+    for pd in pds:
+        q = []
+        for bit in explode_pd(pd):
+            q.extend(bsigs[bit])
         rels = []
         while len(q):
             sig = q.pop()
             rels.append(sig)
-            sigbits = get_simple_bits(sig.expr)
-            nsigs = [ x for nbit in sigbits
-                            for x in bsigs[nbit]
-                                if not (x in q or x in rels) ]
+            sigpds = get_simple_pds(sig.expr)
+            nsigs = []
+            for pd in sigpds:
+                for bit in explode_pd(pd):
+                    for nsig in bsigs[bit]:
+                        if not (nsig in q or nsig in rels):
+                            nsigs.append(nsig)
             q.extend(nsigs)
         nets.append(rels)
     return frozenset([frozenset(rels) for rels in nets])
@@ -233,23 +244,46 @@ def find_networks(pins):
 def print_networks(nets):
     allbits = set()
     for rels in sorted(nets):
-        c = Counter()
+        if len(rels) < 2:
+            continue
+        blah = {}
         for sig in rels:
-            c.update(get_bits(sig.expr))
+            pin = get_sig_pin(pins, sig)
+            if pin not in blah:
+                blah[pin] = []
+            blah[pin].append(sig)
+        c = Counter()
+        for pin, sigs in blah.items():
+            pds = []
+            for sig in sigs:
+                pds.extend(get_pds(sig.expr))
+            c.update(frozenset(pds))
         relbits = frozenset([bit for sig in rels
-            for bit in get_bits(sig.expr)
+            for bit in get_pds(sig.expr)
                 if c[bit] > 1 or sum(c.values()) == len(c)])
         common = [ (bit, c[bit]) for bit in relbits ]
         scommon = sorted(common, key=lambda x: x[0])
         scommon = sorted(scommon, key=lambda x: x[1], reverse=True)
         print("Common bits: {}".format(scommon))
-        defrels = [ (get_sig_pin(pins, sig).other, sig) for sig in rels ]
-        for default, sig in sorted(defrels, key=lambda x: x[0]):
-            selbits = set(get_bits(sig.expr)).difference(relbits)
-            print("('{}', '{}'): {}".format(default, sig.sig, selbits))
+        defrels = blah.items()
+        for pin, sigs in sorted(defrels, key=lambda x: x[0].other):
+            pds = []
+            for sig in sigs:
+                pds.extend(get_pds(sig.expr))
+            selbits = set(pds).difference(relbits)
+            print("('{}', '{}'): {}".format(pin.other,
+                sorted([ x.sig for x in sigs ]), selbits))
         assert allbits.isdisjoint(relbits)
         allbits.update(relbits)
         print()
+
+def find_sig(pins, name):
+    for pin in pins:
+        if pin.high and pin.high.sig == name:
+            return pin.high
+        if pin.low and pin.low.sig == name:
+            return pin.low
+    return None
 
 if __name__ == "__main__":
     pinf = None
