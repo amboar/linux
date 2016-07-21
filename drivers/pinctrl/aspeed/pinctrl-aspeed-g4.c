63b103718d44b2d1ef094163f2377cf527755a06
Andrew Jeffery <andrew@aj.id.au>
2016-07-21 10:29:13 +0930

    fixup: rh: pinctrl-aspeed-g4: Add aspeed,ast2400-pinctrl compat string
    Signed-off-by: Andrew Jeffery <andrew@aj.id.au>

--- drivers/pinctrl/aspeed/pinctrl-aspeed-g4.c
+++ drivers/pinctrl/aspeed/pinctrl-aspeed-g4.c
@@ -1071,6 +1071,7 @@ static int aspeed_g4_pinctrl_probe(struct platform_device *pdev)
 }
 
 static const struct of_device_id aspeed_g4_pinctrl_of_match[] = {
+	{ .compatible = "aspeed,ast2400-pinctrl", },
 	{ .compatible = "aspeed,g4-pinctrl", },
 	{ },
 };
