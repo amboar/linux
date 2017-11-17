522ff7b4d05c06dfb70cef3275d9764b97d5c051
Andrew Jeffery <andrew@aj.id.au>
2017-11-17 17:04:58 +1030

    Revert "fixup: max31785: Remove max31785_get_pwm()"
    This reverts commit 3b947b4c2c61814affabc5e3729fb227ae82f6a4.

--- drivers/hwmon/pmbus/max31785.c
+++ drivers/hwmon/pmbus/max31785.c
@@ -105,6 +105,31 @@ static int max31785_read_long_data(struct i2c_client *client, int page,
 	return rc;
 }
 
+static int max31785_get_pwm(struct i2c_client *client, int page)
+{
+	int config;
+	int command;
+
+	config = pmbus_read_byte_data(client, page, PMBUS_FAN_CONFIG_12);
+	if (config < 0)
+		return config;
+
+	command = pmbus_read_word_data(client, page, PMBUS_FAN_COMMAND_1);
+	if (command < 0)
+		return command;
+
+	if (!(config & PB_FAN_1_RPM)) {
+		if (command >= 0x8000)
+			return 0;
+		else if (command >= 0x2711)
+			return 0x2710;
+
+		return command;
+	}
+
+	return 0;
+}
+
 static int max31785_get_pwm_mode(struct i2c_client *client, int page)
 {
 	int config;
